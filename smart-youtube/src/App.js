930e7892c40fe1e4722943f49078cf316f3fec89
kawnayeen <kawnayeen.08@gmail.com>
2018-03-20 18:47:02 +0600

    youtube search api key added

--- smart-youtube/src/App.js
+++ smart-youtube/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'AIzaSyDyPl-YqhqDwEHEAdSapsJCZgQbD9NFoGs';
+
 class App extends Component {
   render() {
     return (
