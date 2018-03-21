83ac912b9f5ad50e3c7152d1d3e047c980278dcb
kawnayeen <kawnayeen.08@gmail.com>
2018-03-21 18:19:57 +0600

    [smart-youtube] search bar component development started

--- smart-youtube/src/App.js
+++ smart-youtube/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SearchBar from './components/search.bar';
 import './App.css';
 
 const API_KEY = 'AIzaSyDyPl-YqhqDwEHEAdSapsJCZgQbD9NFoGs';
@@ -7,14 +7,8 @@ const API_KEY = 'AIzaSyDyPl-YqhqDwEHEAdSapsJCZgQbD9NFoGs';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SearchBar/>
       </div>
     );
   }
