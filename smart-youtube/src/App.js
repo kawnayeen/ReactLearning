176e1ebe8ddf34bb51ac81777d77ca2c84322942
kawnayeen <kawnayeen.08@gmail.com>
2018-04-03 13:51:54 +0600

    [smart-youtube] clicking on a video from video list update the video details

--- smart-youtube/src/App.js
+++ smart-youtube/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import SearchBar from './components/search.bar';
 import VideoList from './components/video.list';
+import VideoDetail from './components/video.detail';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDyPl-YqhqDwEHEAdSapsJCZgQbD9NFoGs';
@@ -8,15 +9,18 @@ const API_KEY = 'AIzaSyDyPl-YqhqDwEHEAdSapsJCZgQbD9NFoGs';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {videos: []};
-        YTSearch({key: API_KEY, term: 'harun bukeniya'}, videos => this.setState({videos}));
+        this.state = {videos: [], selectedVideo: null};
+        YTSearch({key: API_KEY, term: 'harun bukeniya'}, videos => this.setState({videos:videos, selectedVideo: videos[0]}));
     }
 
     render() {
         return (
             <div>
                 <SearchBar/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>
         );
     }
