52d5d504379a6dc615462180bd8575e7b18fbb22
kawnayeen <kawnayeen.08@gmail.com>
2018-04-03 14:58:23 +0600

    [smart-youtube] searching is hooked up

--- smart-youtube/src/App.js
+++ smart-youtube/src/App.js
@@ -10,13 +10,20 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {videos: [], selectedVideo: null};
-        YTSearch({key: API_KEY, term: 'harun bukeniya'}, videos => this.setState({videos:videos, selectedVideo: videos[0]}));
+        this.videoSearch('harun bukeniya');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, videos => this.setState({
+            videos:videos,
+            selectedVideo: videos[0]
+        }));
     }
 
     render() {
         return (
             <div>
-                <SearchBar/>
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
